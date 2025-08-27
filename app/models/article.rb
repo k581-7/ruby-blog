42857e641378bd7a49c97d20a6a1ff62affde10e
Karla Patricia Farinas <karlafarinas@gmail.com>
2025-08-27 23:55:04 +0800

    Did an activity from class (Article controller) created different parts needed and made a creation article page.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+    validates :name, presence: true, length: { minimum: 3 }
     validates :body, presence: true, length: { minimum: 10 }
 end
